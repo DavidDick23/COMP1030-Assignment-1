98ba2032ef9f3c811bf8ca5aceb180c7008a213d
Dave Dick <60045054+DavidDick23@users.noreply.github.com>
2025-10-07 19:30:51 -0400

    Added the Program.cs file we will collaborating on
    Simply create a new program named Assignment1 on your computer and replace the Program.cs file with this one. We can also use the code space on this site to edit and commit changes.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿#region Script Info
+/*  COMP1030F - Assignment #1
+ *  Group 11: Shy-Anna Lee, Mark Fisher, Suganth Kumaran Vijayavel and David Dick
+ *  
+ *  Description:
+ *  This program is a simple C# console application that helps a user to 
+ *  determine the cost of installing newflooring inside a room in their house.
+ */
+#endregion
+
+using System;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //This is a collaspible region to be completed by a group member
+            #region Completed By #1 <- replace with name
+            //add all of your code and variables in this space 
+            #endregion
+
+            #region Completed By #2
+            //Add all of your code and variables in this space 
+            #endregion
+
+            #region Completed By #3
+            //Add all of your code and variables in this space 
+            #endregion
+
+            #region Completed By #4
+            //Add all of your code and variables in this space 
+            #endregion
+        }
+    }
+}
