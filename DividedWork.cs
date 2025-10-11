c7fdb3480f767b2e6adc816996f60457f98a0f7e
Dave Dick <60045054+DavidDick23@users.noreply.github.com>
2025-10-11 10:31:50 -0400

    Just a suggestion!

---
+++ DividedWork.cs
@@ -0,0 +1,20 @@
+﻿#region Shy
+//Declare and assign three string variables for three different types of flooring each with a price per square foot (perhaps "Hardwood ($8.95/sq.ft.)" as an example - come up with something original, here).
+//Declare a variable to capture the user's choice of flooring type.
+//Declare two variables to represent the length and width of a room (using feet measured to one decimal point).
+//Ask the user the length of the room they are looking to install the new flooring.
+#endregion
+
+#region Mark
+//Check to see that the user has inputted a number - if yes, then store this value in the length variable created in step 4, if no then output an appropriate error message to the console.
+//Ask the user the width of the room they are looking to install the new flooring.
+//Check to see that the user has inputted a number - if yes, then store this value in the length variable created in step 4, if no then output an appropriate error message to the console.
+//Ask the user what type of flooring they would like, by building a sentence using the variables created in step 2 - to make it easy for the user, perhaps assign letters to each choice.
+#endregion
+
+#region Dave
+//Check to see that the user has inputted a letter (one of the three choices for flooring) - if yes, then store this value in the variable created in step 3, if no then output an appropriate error message to the console.
+//Create a series of if() statements that correspond to each choice of flooring.
+//Inside each of the if() statements, do some math: get the area of the floor to be covered, and multiply that by the price of the selected floor type.
+//Output a sentence to the user indicating what floor type they picked, the area of the floor they are covering, and the final cost.
+#endregion
